362f7b46f58a0e72b23c431d7d82fc1e1382ff3e
loveheenavasu <trivediheenalove@gmail.com>
2024-01-17 10:30:16 +0530

    fixed:refactor layout code.

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,6 +1,5 @@
 import { Box, HStack, Stack } from "@chakra-ui/react";
 import React, { ReactElement, memo } from "react";
-import SimpleSidebar from "../SideBar";
 
 interface props {
   children: ReactElement;
@@ -9,7 +8,6 @@ interface props {
 const MainLayout = ({ children, skipAuthCheck = false }: props) => {
   return (
     <Stack h={"100vh"} w={"100vw"} bg={"#0d1325"}>
-      <SimpleSidebar/>
           {children}
         </Stack>
   );
