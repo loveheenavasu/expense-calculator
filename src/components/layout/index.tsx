77dfb3a243069c7b39f129969aa0a76779c9cb55
loveheenavasu <trivediheenalove@gmail.com>
2024-01-17 10:25:39 +0530

    fixed: layout unused code.

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -10,9 +10,7 @@ const MainLayout = ({ children, skipAuthCheck = false }: props) => {
   return (
     <Stack h={"100vh"} w={"100vw"} bg={"#0d1325"}>
       <SimpleSidebar/>
-        {/* <Stack ml={{ sm: "none", base: "none", md: "9.5rem", lg: "9.5rem" }}> */}
           {children}
-          {/* </Stack> */}
         </Stack>
   );
 };
